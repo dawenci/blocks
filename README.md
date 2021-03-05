9f93622a63d72df3fd4608c1900870e8ab004d6e
Xiaowen Ruan <dawenci@gmail.com>
2021-03-05 11:04:45 +0800

    文档重构

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 纯 Web Components 组件库。
 
+## 特性
+
+- 面向未来，使用标准技术
+- 无需构建，直接引入 ES6 js 模块即可
+
+
 ## 文档、Demo
 
-[组件文档](http://dawenci.me/blocks/docs/components.html)
+[组件文档](https://dawenci.me/blocks/docs/components.html)
--- docs/index.js
+++ docs/index.js
@@ -1,30 +1,85 @@
+import routes from './routes.js'
 import '../components/nav-menu/index.js'
 
-const routes = [
-  { title: '安装', url: 'install.html' },
-  { title: '主题', url: 'theme.html' }
-]
+const pageTitle = (route) => {
+  return `${route.category} > ${route.title} - blocksUI web components`
+}
 
 window.onload = () => {
-  const aside = document.querySelector('aside')
-  const iframe = document.querySelector('iframe.article')
+  const $header = document.getElementById('header')
+  const $aside = document.getElementById('aside')
+  const $iframe = document.getElementById('article')
+  
 
-  // 导航
-  const activeRoute = routes.find(route => route.url === decodeURIComponent(location.hash.slice(1))) ?? routes[0]
-  const $menu = aside.appendChild(document.createElement('blocks-nav-menu'))
-  const data = routes.map(route => {
-    return {
-      label: route.title,
+  // 当前激活的路由
+  let activeRoute = routes.find(route => {
+    const hash = decodeURIComponent(location.hash.slice(1))
+    const category = hash.split('/')[0] ?? ''
+    const url = hash.slice(category.length + 1)
+    return route.url === url
+  }) ?? routes[0]
+
+
+  // 当前激活的分类
+  let activeCategory = activeRoute.category
+
+
+  // 页面跳转方法
+  const gotoRoute = (route) => {
+    activeRoute = route
+    activeCategory = route.category
+    $iframe.setAttribute('src', route.url)
+    document.title = pageTitle(route)
+    history.pushState({}, route.title, `#${route.category}/${encodeURIComponent(route.url)}`)
+  }
+
+
+  // 生成侧导航数据
+  const generateSideNavData = () => {
+    return routes
+    .filter(route => route.category === activeCategory)
+    .map(route => {
+      return {
+        label: route.title,
+        active: route === activeRoute,
+        handler() {
+          gotoRoute(route)
+        }
+      }
+    })
+  }
+
+
+  // 侧导航
+  const $sideMenu = $aside.appendChild(document.createElement('blocks-nav-menu'))
+  $sideMenu.style.width = '100%'
+  $sideMenu.data = generateSideNavData()
+
+
+  // 顶导航
+  const $headerMenu = $header.appendChild(document.createElement('blocks-nav-menu'))
+  $headerMenu.horizontal = true
+  $headerMenu.dark = true
+  $headerMenu.size = 'large'
+  const headerNavData = []
+  routes.forEach(route => {
+    if (headerNavData.find(item => item.label === route.category)) {
+      return
+    }
+    headerNavData.push({
+      label: route.category,
       active: route === activeRoute,
       handler() {
-        iframe.setAttribute('src', route.url)
-        history.pushState({}, route.title, `#${encodeURIComponent(route.url)}`)
+        gotoRoute(route)
+        // 分类切换，重新生成侧导航
+        $sideMenu.data = generateSideNavData()
       }
-    }
+    })
   })
-  $menu.style.width = '100%'
-  $menu.data = data
+  $headerMenu.data = headerNavData
 
-  iframe.setAttribute('src', activeRoute.url)
-  document.title = activeRoute.title
+
+  // 初始化加载页面
+  $iframe.setAttribute('src', activeRoute.url)
+  document.title = pageTitle(activeRoute)
 }
