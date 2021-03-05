4fa9aae0c0bb38df810ae0bb23b39fe9846e68da
Xiaowen Ruan <dawenci@gmail.com>
2021-03-05 10:11:07 +0800

    主题注释更新

--- components/theme/var.js
+++ components/theme/var.js
@@ -6,28 +6,38 @@ export const __transition_duration = '.16s'
 
 // --bg-base
 export const __bg_base = '#fff'
+
 // --bg-base-dark
 export const __bg_baseDark = '#333'
 
 // --bg-base-hover
 export const __bg_base_hover = '#f8f8f8'
+
+// --bg-base-dark-hover
 export const __bg_baseDark_hover = '#383838'
 
 // --bg-base-active
 export const __bg_base_active = '#f0f0f0'
+
+// --bg-base-dark-active
 export const __bg_baseDark_active = '#222'
 
 // --fg-base
 export const __fg_base = '#333'
+
 // --fg-base-dark
 export const __fg_baseDark = '#ddd'
 
 // --fg-base-hover
 export const __fg_base_hover = '#222'
+
+// --fg-base-dark-hover
 export const __fg_baseDark_hover = '#eee'
 
 // --fg-base-active
 export const __fg_base_active = '#000'
+
+// --fg-base-dark-active
 export const __fg_baseDark_active = '#fff'
 
 // --fg-secondary
@@ -117,12 +127,18 @@ export const __color_success_dark = '#39b54e'
 
 // --border-color-base
 export const __border_color_base = '#d9d9d9'
+
+// --border-color-base-dark
 export const __border_color_baseDark = '#383838'
 
 // --border-color-light
 export const __border_color_light = '#e5e5e5'
+
+// --border-color-light-dark
 export const __border_color_lightDark = '#444'
 
 // --border-color-disabled
 export const __border_color_disabled = '#d9d9d9'
+
+// --border-color-disabled-dark
 export const __border_color_disabledDark = '#383838'
