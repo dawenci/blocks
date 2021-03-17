523e154f481647e6f93925a8354d5190c5cd7062
Xiaowen Ruan <dawenci@gmail.com>
2021-03-17 16:43:48 +0800

    time 组件重构

--- components/time/index.js
+++ components/time/index.js
@@ -1,5 +1,6 @@
+import '../scrollable/index.js'
 import { dispatchEvent } from '../../common/event.js'
-import { intGetter, intSetter } from '../../common/property.js'
+import { intRangeGetter, intRangeSetter } from '../../common/property.js'
 import { scrollTo } from '../../common/scrollTo.js'
 import { upgradeProperty } from '../../common/upgradeProperty.js'
 import { find, forEach, range } from '../../common/utils.js'
@@ -29,7 +30,7 @@ const TEMPLATE_CSS = `<style>
 
 :host {
   display: inline-block;
-  box-sizing: border-box;
+  vertical-align: top;
   width: calc(var(--height-base, ${__height_base}) * 3 + 12px * 3);
   height: calc(var(--height-base, ${__height_base}) * 8 + 10px);
   background: #fff;
@@ -70,77 +71,84 @@ const TEMPLATE_CSS = `<style>
   top: var(--height-large, ${__height_large});
 }
 
-#layout > ol {
+.col {
   position: relative;
-  height: 100%;
   box-sizing: border-box;
   overflow: hidden;
   float: left;
-  list-style: none;
   width: calc(var(--height-base, ${__height_base}) + 12px);
+  height: 100%;
   margin: 0;
   padding: 0;
 }
-:host([size="small"]) #layout > ol {
+:host([size="small"]) .col {
   width: calc(var(--height-small, ${__height_small}) + 12px);
 }
-:host([size="large"]) #layout > ol {
+:host([size="large"]) .col {
   width: calc(var(--height-large, ${__height_large}) + 12px);
 }
 
-#layout > ol:hover {
-  overflow: auto;
+#layout > .col:hover {
   background: rgba(0,0,0,.025);
 }
-#layout > ol:after {
+#layout > .col:after {
   display: block;
   content: '';
   height: calc(100% - var(--height-base, ${__height_base}));
 }
-:host([size="small"]) #layout > ol:after {
+:host([size="small"]) #layout > .col:after {
   height: calc(100% - var(--height-small, ${__height_small}));
 }
-:host([size="large"]) #layout > ol:after {
+:host([size="large"]) #layout > .col:after {
   height: calc(100% - var(--height-large, ${__height_large}));
 }
 
 #hours {}
 #minutes {}
 #seconds {}
-li {
+.item {
   height: var(--height-base, ${__height_base});
   line-height: var(--height-base, ${__height_base});
-  padding: 0 0 0 12px;
+  text-align: center;
   cursor: default;
 }
-:host([size="small"]) li {
+:host([size="small"]) .item {
   height: var(--height-small, ${__height_small});
   line-height: var(--height-small, ${__height_small});
 }
-:host([size="large"]) li {
+:host([size="large"]) .item {
   height: var(--height-large, ${__height_large});
   line-height: var(--height-large, ${__height_large});
 }
 
-li:hover {
+.item:hover {
   font-weight: 700;
   background-color: #f0f0f0;
 }
 
-li:active,
-li.active,
-li.active:hover {
+.item:active,
+.item.active,
+.item.active:hover {
   font-weight: 700;
   color: var(--color-primary, ${__color_primary});
 }
 
+.bot {
+  height: calc(100% - var(--height-base, ${__height_base}));
+}
+:host([size="small"]) .bot {
+  height: calc(100% - var(--height-small, ${__height_small}));
+}
+:host([size="large"]) .bot {
+  height: calc(100% - var(--height-large, ${__height_large}));
+}
 </style>`
 
 const TEMPLATE_HTML = `
 <div id="layout">
-  <ol id="hours"></ol>
-  <ol id="minutes"></ol>
-  <ol id="seconds"></ol>
+  <bl-scrollable class="col" id="hours"></bl-scrollable>
+  <bl-scrollable class="col" id="minutes"></bl-scrollable>
+  <bl-scrollable class="col" id="seconds"></bl-scrollable>
 </div>
 `
 
@@ -164,9 +172,15 @@ class BlocksTime extends HTMLElement {
     const handler = (prop) => {
       return (e) => {
         const target = e.target
-        if (target.tagName === 'LI') {
+        if (target.classList.contains('item')) {
           const value = +target.textContent
           this[prop] = value
+
+          requestAnimationFrame(() => {
+            forEach(this.$layout.querySelectorAll('.active'), $active => {
+              scrollTo($active.parentElement, $active.offsetTop, { duration: .16 })
+            })
+          })
         }
       }
     }
@@ -176,47 +190,56 @@ class BlocksTime extends HTMLElement {
   }
 
   get hour() {
-    return intGetter('hour')(this)
+    return intRangeGetter('hour', 0, 23)(this)
   }
 
   set hour(value) {
-    intSetter('hour')(this, value)
+    intRangeSetter('hour', 0, 23)(this, value)
   }
 
   get minute() {
-    return intGetter('minute')(this)
+    return intRangeGetter('minute', 0, 59)(this)
   }
 
   set minute(value) {
-    intSetter('minute')(this, value)
+    intRangeSetter('minute', 0, 59)(this, value)
   }
 
   get second() {
-    return intGetter('second')(this)
+    return intRangeGetter('second', 0, 59)(this)
   }
 
   set second(value) {
-    intSetter('second')(this, value)
+    intRangeSetter('second', 0, 59)(this, value)
   }
 
   render() {
     if (!this.$hours.children.length) {
       range(0, 23).forEach(n => {
-        const $li = this.$hours.appendChild(document.createElement('li'))
-        $li.textContent = n < 10 ? '0' + n : n
+        const $item = this.$hours.appendChild(document.createElement('div'))
+        $item.className = 'item'
+        $item.textContent = n < 10 ? '0' + n : n
       })
+      const $bot = this.$hours.appendChild(document.createElement('div'))
+      $bot.className = 'bot'
     }
     if (!this.$minutes.children.length) {
       range(0, 59).forEach(n => {
-        const $li = this.$minutes.appendChild(document.createElement('li'))
-        $li.textContent = n < 10 ? '0' + n : n
+        const $item = this.$minutes.appendChild(document.createElement('div'))
+        $item.className = 'item'
+        $item.textContent = n < 10 ? '0' + n : n
       })
+      const $bot = this.$minutes.appendChild(document.createElement('div'))
+      $bot.className = 'bot'
     }
     if (!this.$seconds.children.length) {
       range(0, 59).forEach(n => {
-        const $li = this.$seconds.appendChild(document.createElement('li'))
-        $li.textContent = n < 10 ? '0' + n : n
+        const $item = this.$seconds.appendChild(document.createElement('div'))
+        $item.className = 'item'
+        $item.textContent = n < 10 ? '0' + n : n
       })
+      const $bot = this.$seconds.appendChild(document.createElement('div'))
+      $bot.className = 'bot'
     }
   }
 
@@ -252,7 +275,6 @@ class BlocksTime extends HTMLElement {
         const $new = find($list.children, $li => +$li.textContent === +newVal)
         if ($new) {
           $new.classList.add('active')
-          scrollTo($list, $new.offsetTop, { duration: .16 })
         }
   
         if (attrName !== 'hour' && !this.hour) this.hour = 0
