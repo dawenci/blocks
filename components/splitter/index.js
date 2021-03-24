c6980da8908ceb07799ea02298afefe5e0998443
Xiaowen Ruan <dawenci@gmail.com>
2021-03-24 16:24:23 +0800

    splitter 调整尺寸逻辑重构。

--- components/splitter/index.js
+++ components/splitter/index.js
@@ -49,6 +49,77 @@ const TEMPLATE = `<style>
   flex-direction: column;
 }
 
+.handle {
+  box-sizing: border-box;
+  position: absolute;
+  display: block;
+  user-select: none;
+}
+.handle.active,
+.handle:hover,
+.handle.dragging {
+  z-index: 1;
+}
+.handle::before {
+  position: absolute;
+  content: '';
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 100%;
+  height: 100%;
+}
+.handle::after {
+  position: absolute;
+  content: '';
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 1px;
+  height: 1px;
+}
+.handle:hover::before,
+.handle.dragging::before {
+  background-color: var(--color-primary-light, ${__color_primary_light});
+  opacity: .5;
+}
+.handle:hover::after,
+.handle.dragging::after {
+  background-color: var(--color-primary-light, ${__color_primary_light});
+}
+
+:host(:not([direction])) .handle,
+:host([direction="row"]) .handle {
+  top: 0;
+  right: auto;
+  bottom: auto;
+  left: 0;
+  height: 100%;
+  border-top: 0;
+  border-bottom: 0;
+  cursor: col-resize;
+}
+:host(:not([direction])) .handle::after,
+:host([direction="row"]) .handle::after {
+  height: 100%;
+}
+:host([direction="column"]) .handle {
+  top: 0;
+  right: auto;
+  bottom: auto;
+  left: 0;
+  width: 100%;
+  border-left: 0;
+  border-right: 0;
+  cursor: row-resize;
+}
+:host([direction="column"]) .handle::after {
+  width: 100%;
+}
 </style>
 
 <div id="layout">
@@ -80,81 +151,17 @@ const PANE_TEMPLATE = `<style>
   margin: 0;
   padding: 0;
 }
-#handle {
-  box-sizing: border-box;
-  position: absolute;
-  display: block;
-  user-select: none;
-  border: 1px solid rgba(0,0,0,.1);
-  background-color: rgba(0,0,0,.025);
-}
-#handle::before {
-  position: absolute;
-  content: '';
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
-  width: 2px;
-  height: 2px;
-  border-radius: 2px;
-}
-#handle:hover,
-#handle.dragging {
-  border-color: var(--color-primary-light, ${__color_primary});
-  background-color: var(--color-primary-light, ${__color_primary_light});
-}
-#handle:hover::before,
-#handle.dragging::before {
-  background-color: #fff;
-}
-
-:host(.row) #handle {
-  top: 0;
-  right: auto;
-  bottom: auto;
-  left: 0;
-  height: 100%;
-  border-top: 0;
-  border-bottom: 0;
-  cursor: col-resize; 
-}
-:host(.row) #handle::before {
-  height: 30px;
-}
-:host(.column) #handle {
-  top: 0;
-  right: auto;
-  bottom: auto;
-  left: 0;
-  width: 100%;
-  border-left: 0;
-  border-right: 0;
-  cursor: row-resize;
-}
-:host(.column) #handle::before {
-  width: 30px;
-}
-/* 第一个一个面板不显示拖动柄 */
-:host(:first-child) #handle {
-  display: none;
-}
-:host(.row:first-child) #handle {
-  padding-left: 0;
-}
-:host(.column:first-child) #handle {
-  padding-top: 0;
-}
 </style>
 <div id="content" part="content"><slot></slot></div>
-<div id="handle" part="handle"></div>
 `
 
 const template = document.createElement('template')
 const paneTemplate = document.createElement('template')
 template.innerHTML = TEMPLATE
 paneTemplate.innerHTML = PANE_TEMPLATE
+
+const $handleTemplate = document.createElement('div')
+$handleTemplate.className = 'handle'
 
 export class BlocksSplitter extends HTMLElement {
   static get observedAttributes() {
@@ -165,6 +172,7 @@ export class BlocksSplitter extends HTMLElement {
     super()
     const shadowRoot = this.attachShadow({mode: 'open'})
     shadowRoot.appendChild(template.content.cloneNode(true))
+    this.$layout = shadowRoot.getElementById('layout')
     this.$panes = shadowRoot.getElementById('panes')
     this.$cover = shadowRoot.getElementById('cover')
     this.$slot = this.$panes.querySelector('slot')
@@ -176,9 +184,11 @@ export class BlocksSplitter extends HTMLElement {
       this.panes.forEach($pane => {
         $pane.classList.toggle('row', this.direction === 'row')
         $pane.classList.toggle('column', this.direction === 'column')
-        $pane.updateStyle()
       })
+      this.layout()
     })
+
+    this._initResizeEvents()
   }
 
   get direction() {
@@ -220,10 +230,29 @@ export class BlocksSplitter extends HTMLElement {
 
   attributeChangedCallback(attrName, oldVal, newVal) {}
 
+  renderHandles() {
+    const count = this.panes.length - 1
+    let len = this.$layout.querySelectorAll('.handle').length
+    while (len++ < count) {
+      this.$layout.insertBefore($handleTemplate.cloneNode(true), this.$cover)
+    }
+    len = this.$layout.querySelectorAll('.handle').length
+    while (len-- > count) {
+      this.$layout.removeChild(this.$layout.querySelector('.handle'))
+    }
+    this.handles = Array.prototype.slice.call(this.$layout.querySelectorAll('.handle'))
+    this.handles.forEach(($handle, index) => {
+      const offset = this.getPanePosition(this.panes[index + 1]) - this.handleSize / 2
+      const sizeProp = this.direction === 'row' ? 'width' : 'height'
+      const posProp = this.direction === 'row' ? 'left' : 'top'
+      $handle.style.cssText = `${sizeProp}:${this.handleSize}px;${posProp}:${offset}px;`
+    })
+  }
+
   /** 计算 pane 的尺寸 */
   getPaneSize($pane) {
     let size = $pane.size || 0
-    size = Math.max(size, $pane.min, this.panes[0] === $pane ? 0 : this.handleSize)
+    size = Math.max(size, $pane.min)
     size = Math.min(size, $pane.max)
     return size
   }
@@ -275,16 +304,15 @@ export class BlocksSplitter extends HTMLElement {
     // 当前面板与上一个面板的尺寸合计
     const totalSize = $pane.size + $prevPane.size
 
-    const handleSize = this.handleSize
     const splitterSize = this.size
 
     // 确保调整后的尺寸不溢出
-    // 1. 面板不能小于拖拽柄，也不能小于面板的 min 设置，上一个面板不能因此大于上个面板的 max
+    // 1. 面板不能小于面板的 min 设置，上一个面板不能因此大于上个面板的 max
     // 2. 面板不能大于外容器，也不能大于面板的 max 设置，上一个面板不能因此小于上个面板的 min
-    const min = Math.max(handleSize, $pane.min || handleSize)
-    const max = Math.min(splitterSize, $pane.max || splitterSize)
-    const prevMin = Math.max((index === 1 ? 0 : handleSize), $prevPane?.min ?? 0)
-    const prevMax = Math.min(splitterSize, $prevPane?.max ?? splitterSize)
+    const min = $pane.min
+    const max = Math.min(splitterSize, $pane.max)
+    const prevMin = $prevPane.min
+    const prevMax = Math.min(splitterSize, $prevPane.max)
 
     let prevNewSize = totalSize - newSize
 
@@ -357,9 +385,10 @@ export class BlocksSplitter extends HTMLElement {
     // 未分配的尺寸（容器宽高 - 已分配的宽高）
     const rest = this.size - sum
 
-
     // 没有未分配的尺寸，无需调整
     if (rest === 0) {
+      this.panes.forEach($pane => $pane.updateStyle())
+      this.renderHandles()
       dispatchEvent(this, 'layout')
       return
     }
@@ -373,22 +402,100 @@ export class BlocksSplitter extends HTMLElement {
 
     // 未分配的尺寸小于 0，说明收缩了，需要将这些收缩量分配到 pane 上
     this._shrinkPanes(-rest, this.panes)
-
     dispatchEvent(this, 'layout')
+  }
+
+  setActiveHandle($pane) {
+    this.clearActiveHandle()
+    let index = this.getPaneIndex($pane)
+    this.handles[index] && this.handles[index].classList.add('active')
+    this.handles[index - 1] && this.handles[index - 1].classList.add('active')
+  }
+
+  clearActiveHandle() {
+    this.handles.forEach($handle => $handle.classList.remove('active'))
+  }
+
+  gethandleIndex($handle) {
+    return Array.prototype.indexOf.call(this.$layout.querySelectorAll('.handle'), $handle)
+  }
+
+  _initResizeEvents() {
+    // 开始拖拽调整面板尺寸时，鼠标的坐标，以及面板宽度初始值
+    // 用于计算偏移
+    let resizeStart = { x: 0, y: 0, size: 0 }
+
+    // 拖拽调整面板尺寸过程，鼠标坐标
+    let mouseCurrent = { x: 0, y: 0 }
+
+    let $handle = null
+    let $pane = null
+
+    const clear = () => {
+      document.removeEventListener('mousemove', onmove)
+      document.removeEventListener('mouseup', onup)
+      this.toggleCover(false)
+      $handle.classList.remove('dragging')
+      $handle = null
+      $pane = null
+    }
+
+    const onmove = e => {
+      mouseCurrent = { x: e.pageX, y: e.pageY }
+      // 鼠标离开窗口的时候，退出
+      const $from = e.relatedTarget || e.toElement
+      if (!$from || $from.nodeName === 'HTML') {
+        clear()
+        return
+      }
+
+      // 刷新
+      const axis = this.direction === 'row' ? 'x' : 'y'
+      const mouseOffset = mouseCurrent[axis] - resizeStart[axis]
+      const newSize = resizeStart.size - mouseOffset
+      this.resizePane($pane, newSize)
+      this.renderHandles()
+    }
+
+    const onup = () => {
+      clear()
+    }
+
+    const onstart = (e) => {
+      if (!e.target.classList.contains('handle')) return
+      $handle = e.target
+      $pane = this.panes[this.gethandleIndex($handle) + 1]
+      resizeStart = {
+        x: e.pageX,
+        y: e.pageY,
+        size: $pane.size,
+      }
+      mouseCurrent = {
+        x: e.pageX,
+        y: e.pageY
+      }
+
+      document.addEventListener('mousemove', onmove)
+      document.addEventListener('mouseup', onup)
+      this.toggleCover(true)
+      $handle.classList.add('dragging')
+    }
+
+    this.$layout.onmousedown = onstart
   }
 
   // 获取面板允许扩张的尺寸
   _getGrowSize($pane) {
     if (this.isSizeFrozen($pane)) return 0
     if ($pane.grow <= 0) return 0
-    return $pane.max - Math.max($pane.size, this.handleSize)
+    return $pane.max - $pane.size
   }
 
   // 获取面板允许收缩的尺寸
   _getShrinkSize($pane) {
     if (this.isSizeFrozen($pane)) return 0
     if ($pane.shrink <= 0) return 0
-    return $pane.max - Math.max($pane.size, this.handleSize)
+    return $pane.max - $pane.size
   }
 
   // 将 rest 尺寸分配到 panes 上
@@ -419,6 +526,7 @@ export class BlocksSplitter extends HTMLElement {
     loop(rest, panes)
     if (refresh) {
       panes.forEach($pane => $pane.updateStyle())
+      this.renderHandles()
     }
   }
 
@@ -450,6 +558,7 @@ export class BlocksSplitter extends HTMLElement {
     loop(rest, panes)
     if (refresh) {
       panes.forEach($pane => $pane.updateStyle())
+      this.renderHandles()
     }
   }
 
@@ -469,7 +578,7 @@ export class BlocksSplitterPane extends HTMLElement {
       // 最大尺寸
       'max',
       // 最小尺寸
-      'min',      
+      'min',
       // 弹性尺寸收缩率
       'shrink',
     ]
@@ -479,60 +588,9 @@ export class BlocksSplitterPane extends HTMLElement {
     super()
     const shadowRoot = this.attachShadow({mode: 'open'})
     shadowRoot.appendChild(paneTemplate.content.cloneNode(true))
-    this.$content = shadowRoot.getElementById('content')
-    this.$handle = shadowRoot.getElementById('handle')
-
-    // 开始拖拽调整面板尺寸时，鼠标的坐标，以及面板宽度初始值
-    // 用于计算偏移
-    let resizeStart = { x: 0, y: 0, size: 0 }
-
-    // 拖拽调整面板尺寸过程，鼠标坐标
-    let mouseCurrent = { x: 0, y: 0 }
-
-    const clear = () => {
-      document.removeEventListener('mousemove', onmove)
-      document.removeEventListener('mouseup', onup)
-      this.$splitter.toggleCover(false)
-      this.$handle.classList.remove('dragging')
-    }
-
-    const onmove = e => {
-      mouseCurrent = { x: e.pageX, y: e.pageY }
-      // 鼠标离开窗口的时候，退出
-      const $from = e.relatedTarget || e.toElement
-      if (!$from || $from.nodeName === 'HTML') {
-        clear()
-      }
-
-      // 刷新
-      const axis = this.$splitter.direction === 'row' ? 'x' : 'y'
-      const mouseOffset = mouseCurrent[axis] - resizeStart[axis]
-      const newSize = resizeStart.size - mouseOffset
-      this.$splitter.resizePane(this, newSize)
-    }
-
-    const onup = () => {
-      clear()
-    }
-
-    const onstart = (e) => {
-      resizeStart = {
-        x: e.pageX,
-        y: e.pageY,
-        size: this.size,
-      }
-      mouseCurrent = {
-        x: e.pageX,
-        y: e.pageY
-      }
-
-      document.addEventListener('mousemove', onmove)
-      document.addEventListener('mouseup', onup)
-      this.$splitter.toggleCover(true)
-      this.$handle.classList.add('dragging')
-    }
-
-    this.$handle.onmousedown = onstart
+    this.addEventListener('mouseenter', () => {
+      this.$splitter.setActiveHandle(this)
+    })
   }
 
   get basis() {
@@ -588,23 +646,11 @@ export class BlocksSplitterPane extends HTMLElement {
   }
 
   updateStyle() {
-    const isFirstPane = this.$splitter.panes[0] === this
     const sizeProp = this.$splitter.direction === 'row' ? 'width' : 'height'
     const posProp = this.$splitter.direction === 'row' ? 'left' : 'top'
-
     // 宽度/高度 样式计算，外框宽度 - 拖动柄的宽度（如果显示拖动柄）
-    this.$content.style[sizeProp] = isFirstPane
-      ? this.$splitter.getPaneSize(this) + 'px'
-      : (this.$splitter.getPaneSize(this) - this.$splitter.handleSize) + 'px'
-
+    this.style[sizeProp] = this.$splitter.getPaneSize(this) + 'px'
     this.style[posProp] = this.$splitter.getPanePosition(this) + 'px'
-    if (!isFirstPane) {
-      this.style['padding-' + posProp] = this.$splitter.handleSize + 'px'
-    }
-
-    // 设置 handle
-    this.$handle.style.width = this.$handle.style.height = ''
-    this.$handle.style[sizeProp] = isFirstPane ? '0' : this.$splitter.handleSize + 'px'
   }
 
   render() {}
