6d2f6755086e399d75ed24edb5f9d587695b2b5c
Xiaowen Ruan <dawenci@gmail.com>
2021-03-24 10:18:21 +0800

    Splitter 组件实现。

--- components/splitter/index.js
+++ components/splitter/index.js
@@ -1,5 +1,5 @@
 import { dispatchEvent } from '../../common/event.js'
-import { boolGetter, boolSetter, enumGetter, enumSetter, intGetter, intSetter, numGetter, numSetter } from '../../common/property.js'
+import { enumGetter, enumSetter, intGetter, intSetter, numGetter, numSetter } from '../../common/property.js'
 import { sizeObserve } from '../../common/sizeObserve.js'
 import { upgradeProperty } from '../../common/upgradeProperty.js'
 import { __border_color_base, __border_color_light, __color_primary, __color_primary_light } from '../../theme/var.js'
@@ -151,16 +151,14 @@ const PANE_TEMPLATE = `<style>
 <div id="handle" part="handle"></div>
 `
 
-
 const template = document.createElement('template')
 const paneTemplate = document.createElement('template')
 template.innerHTML = TEMPLATE
 paneTemplate.innerHTML = PANE_TEMPLATE
 
-
 export class BlocksSplitter extends HTMLElement {
   static get observedAttributes() {
-    return ['direction', 'handleSize']
+    return ['direction', 'handle-size']
   }
 
   constructor() {
@@ -171,20 +169,11 @@ export class BlocksSplitter extends HTMLElement {
     this.$cover = shadowRoot.getElementById('cover')
     this.$slot = this.$panes.querySelector('slot')
 
-    this.splitterStore = {
-      // 显示遮罩，这是为了避免某个 pane 里面存在 iframe， 捕获了鼠标，导致 mousemove 事件无法正确工作
-      cover: false,
-      width: 0,
-      height: 0,
-      panes: [],
-      prop: this.direction === 'row' ? 'width' : 'height',
-      from: this.direction === 'row' ? 'left' : 'top',
-      axis: this.direction === 'row' ? 'x' : 'y',
-    }
+    this.panes = []
 
     this.$slot.addEventListener('slotchange', e => {
-      this.splitterStore.panes = this.$slot.assignedElements()
-      this.splitterStore.panes.forEach($pane => {
+      this.panes = this.$slot.assignedElements()
+      this.panes.forEach($pane => {
         $pane.classList.toggle('row', this.direction === 'row')
         $pane.classList.toggle('column', this.direction === 'column')
         $pane.updateStyle()
@@ -208,14 +197,33 @@ export class BlocksSplitter extends HTMLElement {
     intSetter('handle-size')(this, value)
   }
 
-  updatePaneStyle($pane) {
-    $pane.updateStyle()
+  get size() {
+    return this.$panes[this.direction === 'row' ? 'clientWidth' : 'clientHeight']
   }
+
+  render() {}
+
+  connectedCallback() {
+    this.constructor.observedAttributes.forEach(attr => {
+      upgradeProperty(this, attr)
+    })
+    this.render()
+
+    this._offSizeObserve = sizeObserve(this, this.layout.bind(this))
+  }
+
+  disconnectedCallback() {
+    this._offSizeObserve()
+  }
+
+  adoptedCallback() {}
+
+  attributeChangedCallback(attrName, oldVal, newVal) {}
 
   /** 计算 pane 的尺寸 */
   getPaneSize($pane) {
     let size = $pane.size || 0
-    size = Math.max(size, $pane.min, this.splitterStore.panes[0] === $pane ? 0 : this.handleSize)
+    size = Math.max(size, $pane.min, this.panes[0] === $pane ? 0 : this.handleSize)
     size = Math.min(size, $pane.max)
     return size
   }
@@ -227,10 +235,10 @@ export class BlocksSplitter extends HTMLElement {
 
   /** 计算 pane 的定位 */
   getPanePosition($pane) {
-    if (this.splitterStore && this.splitterStore.panes) {
-      const index = this.splitterStore.panes.indexOf($pane)
+    if (this.panes.length) {
+      const index = this.panes.indexOf($pane)
       if (index !== -1) {
-        return this.splitterStore.panes.slice(0, index)
+        return this.panes.slice(0, index)
           .reduce((acc, pane) => {
             return acc + this.getPaneSize(pane)
           }, 0)
@@ -245,13 +253,13 @@ export class BlocksSplitter extends HTMLElement {
   }
 
   /** 获取当前是第几个面板 */
-  getPaneIndex(pane) {
-    return this.splitterStore.panes.indexOf(pane)
+  getPaneIndex($pane) {
+    return this.panes.indexOf($pane)
   }
 
   /** 调整 pane 的尺寸 */
   resizePane($pane, newSize) {
-    const panes = this.splitterStore.panes
+    const panes = this.panes
     // 面板是冻结的的，意味着不能调整尺寸，退出
     if (this.isSizeFrozen($pane)) return
 
@@ -260,23 +268,23 @@ export class BlocksSplitter extends HTMLElement {
     const index = panes.indexOf($pane)
     if (index < 1) return
 
-    const prevPane = panes[index - 1]
+    const $prevPane = panes[index - 1]
     // 上一个面板是冻结的的，意味着不能调整尺寸，退出
-    if (this.isSizeFrozen(prevPane)) return
+    if (this.isSizeFrozen($prevPane)) return
 
     // 当前面板与上一个面板的尺寸合计
-    const totalSize = $pane.size + prevPane.size
+    const totalSize = $pane.size + $prevPane.size
 
     const handleSize = this.handleSize
-    const splitterSize = this.splitterStore[this.splitterStore.prop]
+    const splitterSize = this.size
 
     // 确保调整后的尺寸不溢出
     // 1. 面板不能小于拖拽柄，也不能小于面板的 min 设置，上一个面板不能因此大于上个面板的 max
     // 2. 面板不能大于外容器，也不能大于面板的 max 设置，上一个面板不能因此小于上个面板的 min
     const min = Math.max(handleSize, $pane.min || handleSize)
     const max = Math.min(splitterSize, $pane.max || splitterSize)
-    const prevMin = Math.max((index === 1 ? 0 : handleSize), prevPane?.min ?? 0)
-    const prevMax = Math.min(splitterSize, prevPane?.max ?? splitterSize)
+    const prevMin = Math.max((index === 1 ? 0 : handleSize), $prevPane?.min ?? 0)
+    const prevMax = Math.min(splitterSize, $prevPane?.max ?? splitterSize)
 
     let prevNewSize = totalSize - newSize
 
@@ -299,22 +307,22 @@ export class BlocksSplitter extends HTMLElement {
 
     const offset = newSize - $pane.size
     $pane.size = newSize
-    prevPane.size = prevNewSize
+    $prevPane.size = prevNewSize
 
-    prevPane.updateStyle()
+    $prevPane.updateStyle()
     $pane.updateStyle()
 
-    dispatchEvent(this, 'pane-resized', { detail: { offset, pane: $pane, prevPane } } )
+    dispatchEvent(this, 'pane-resized', { detail: { offset, $pane, $prevPane } } )
 
     // 往 右/下 拖拽
     if (offset < 0) {
       // 面板完全折叠
       if ($pane.size <= min) {
-        dispatchEvent(this, 'pane-close', { detail: { pane: $pane } })
+        dispatchEvent(this, 'pane-close', { detail: { $pane } })
       }
       // 前一个面板从折叠到展开
-      if (prevPane.size === prevMin - offset) {
-        dispatchEvent(this, 'pane-open', { detail: { pane: prevPane } })
+      if ($prevPane.size === prevMin - offset) {
+        dispatchEvent(this, 'pane-open', { detail: { $pane: $prevPane } })
       }
     }
 
@@ -322,11 +330,11 @@ export class BlocksSplitter extends HTMLElement {
     if (offset > 0) {
       // 面板从折叠到展开
       if ($pane.size === offset + min) {
-        dispatchEvent(this, 'pane-open', { detail: { pane: $pane } })
+        dispatchEvent(this, 'pane-open', { detail: { $pane } })
       }
       // 前一个面板完全折叠
-      if (prevPane.size <= prevMin) {
-        dispatchEvent(this, 'pane-close', { detail: { pane: prevPane } })
+      if ($prevPane.size <= prevMin) {
+        dispatchEvent(this, 'pane-close', { detail: { $pane: $prevPane } })
       }
     }
   }
@@ -343,39 +351,30 @@ export class BlocksSplitter extends HTMLElement {
   }
 
   layout() {
-    const splitterStore = this.splitterStore
-    // 容器宽高
-    this.splitterStore.width = this.$panes.clientWidth
-    this.splitterStore.height = this.$panes.clientHeight
-
-    // 确认将要调整的是 pane 的 width 还是 height
-    const prop = splitterStore.prop
-
     // 统计当前所有 pane 的尺寸总和
-    const sum = splitterStore.panes.reduce((acc, pane) => {
-      return acc + pane.size
-    }, 0)
+    const sum = this.panes.reduce((acc, $pane) => acc + $pane.size, 0)
 
-    // 未分配的尺寸
-    const rest = splitterStore[prop] - sum
+    // 未分配的尺寸（容器宽高 - 已分配的宽高）
+    const rest = this.size - sum
+
 
     // 没有未分配的尺寸，无需调整
     if (rest === 0) {
-      dispatchEvent(this, 'layout', { detail: { store: this.splitterStore } } )
+      dispatchEvent(this, 'layout')
       return
     }
 
     // 未分配的尺寸大于 0，说明扩张了，则需要将这些尺寸加在各个 pane 上
     if (rest > 0) {
-      this._growPanes(rest, splitterStore.panes)
-      dispatchEvent(this, 'layout', { detail: { store: this.splitterStore } } )
+      this._growPanes(rest, this.panes)
+      dispatchEvent(this, 'layout')
       return
     }
 
     // 未分配的尺寸小于 0，说明收缩了，需要将这些收缩量分配到 pane 上
-    this._shrinkPanes(-rest, splitterStore.panes)
+    this._shrinkPanes(-rest, this.panes)
 
-    dispatchEvent(this, 'layout', { detail: { store: this.splitterStore } } )
+    dispatchEvent(this, 'layout')
   }
 
   // 获取面板允许扩张的尺寸
@@ -393,14 +392,12 @@ export class BlocksSplitter extends HTMLElement {
   }
 
   // 将 rest 尺寸分配到 panes 上
-  _growPanes(rest, $panes) {
+  _growPanes(rest, panes) {
     let refresh = false
     // 递归处理，返回一趟处理完毕剩余未分配的尺寸
-    const loop = (rest, $panes) => {
+    const loop = (rest, panes) => {
       // 找出能接纳扩张的 pane
-      const $list = $panes.filter($pane => {
-        return this._getGrowSize($pane) >= 1
-      })
+      const $list = panes.filter($pane => this._getGrowSize($pane) >= 1)
       if (!$list.length) return
       refresh = true
 
@@ -419,20 +416,18 @@ export class BlocksSplitter extends HTMLElement {
         loop(rest, $list)
       }
     }
-    loop(rest, $panes)
+    loop(rest, panes)
     if (refresh) {
-      $panes.forEach($pane => $pane.updateStyle())
+      panes.forEach($pane => $pane.updateStyle())
     }
   }
 
-  _shrinkPanes(rest, $panes) {
+  _shrinkPanes(rest, panes) {
     let refresh = false
     // 递归处理，返回一趟处理完毕剩余未分配的尺寸
     const loop = (rest, panes) => {
       // 找出能接纳收缩的 pane
-      const $list = panes.filter(pane => {
-        return this._getShrinkSize(pane) >= 1
-      })
+      const $list = panes.filter($pane => this._getShrinkSize($pane) >= 1)
       if (!$list.length) return
       refresh = true
 
@@ -452,60 +447,29 @@ export class BlocksSplitter extends HTMLElement {
         loop(rest, $list)
       }
     }
-    loop(rest, $panes)
+    loop(rest, panes)
     if (refresh) {
-      $panes.forEach($pane => $pane.updateStyle())
+      panes.forEach($pane => $pane.updateStyle())
     }
   }
 
-  _onDirectionChange(value) {
-    this.splitterStore.prop = value === 'row' ? 'width' : 'height'
-    this.splitterStore.from = value === 'row' ? 'left' : 'top'
-    this.splitterStore.axis = value === 'row' ? 'x' : 'y'
-  }
-
+  // 显示遮罩，这是为了避免某个 pane 里面存在 iframe， 捕获了鼠标，导致 mousemove 事件无法正确工作
   toggleCover(visible) {
     this.$cover.style.display = visible ? 'block' : 'none'
   }
-
-  render() {}
-
-  connectedCallback() {
-    this.constructor.observedAttributes.forEach(attr => {
-      upgradeProperty(this, attr)
-    })
-    this.render()
-
-    this.layout()
-    this._offSizeObserve = sizeObserve(this, this.layout.bind(this))
-  }
-
-  disconnectedCallback() {
-    this._offSizeObserve()
-  }
-
-  adoptedCallback() {}
-
-  attributeChangedCallback(attrName, oldVal, newVal) {
-    if (attrName === 'direction') {
-      this._onDirectionChange()
-    }
-  }
 }
-
-
 
 export class BlocksSplitterPane extends HTMLElement {
   static get observedAttributes() {
     return [
-      // 最大尺寸
-      'max',
-      // 最小尺寸
-      'min',
       // 弹性尺寸基础值
       'basis',
       // 弹性尺寸增长率
       'grow',
+      // 最大尺寸
+      'max',
+      // 最小尺寸
+      'min',      
       // 弹性尺寸收缩率
       'shrink',
     ]
@@ -520,17 +484,10 @@ export class BlocksSplitterPane extends HTMLElement {
 
     // 开始拖拽调整面板尺寸时，鼠标的坐标，以及面板宽度初始值
     // 用于计算偏移
-    this.resizeStart = {
-      x: 0,
-      y: 0,
-      size: 0,
-    }
+    let resizeStart = { x: 0, y: 0, size: 0 }
 
     // 拖拽调整面板尺寸过程，鼠标坐标
-    this.mouseCurrent = {
-      x: 0,
-      y: 0,
-    }
+    let mouseCurrent = { x: 0, y: 0 }
 
     const clear = () => {
       document.removeEventListener('mousemove', onmove)
@@ -540,17 +497,17 @@ export class BlocksSplitterPane extends HTMLElement {
     }
 
     const onmove = e => {
-      this.mouseCurrent = { x: e.pageX, y: e.pageY }
+      mouseCurrent = { x: e.pageX, y: e.pageY }
       // 鼠标离开窗口的时候，退出
-      const from = e.relatedTarget || e.toElement
-      if (!from || from.nodeName === 'HTML') {
+      const $from = e.relatedTarget || e.toElement
+      if (!$from || $from.nodeName === 'HTML') {
         clear()
       }
 
       // 刷新
-      const axis = this.$splitter.splitterStore.axis
-      const mouseOffset = this.mouseCurrent[axis] - this.resizeStart[axis]
-      const newSize = this.resizeStart.size - mouseOffset
+      const axis = this.$splitter.direction === 'row' ? 'x' : 'y'
+      const mouseOffset = mouseCurrent[axis] - resizeStart[axis]
+      const newSize = resizeStart.size - mouseOffset
       this.$splitter.resizePane(this, newSize)
     }
 
@@ -559,12 +516,12 @@ export class BlocksSplitterPane extends HTMLElement {
     }
 
     const onstart = (e) => {
-      this.resizeStart = {
+      resizeStart = {
         x: e.pageX,
         y: e.pageY,
         size: this.size,
       }
-      this.mouseCurrent = {
+      mouseCurrent = {
         x: e.pageX,
         y: e.pageY
       }
@@ -631,22 +588,23 @@ export class BlocksSplitterPane extends HTMLElement {
   }
 
   updateStyle() {
-    const store = this.$splitter.splitterStore
-    const isFirstPane = store.panes[0] === this
+    const isFirstPane = this.$splitter.panes[0] === this
+    const sizeProp = this.$splitter.direction === 'row' ? 'width' : 'height'
+    const posProp = this.$splitter.direction === 'row' ? 'left' : 'top'
 
     // 宽度/高度 样式计算，外框宽度 - 拖动柄的宽度（如果显示拖动柄）
-    this.$content.style[store.prop] = isFirstPane
+    this.$content.style[sizeProp] = isFirstPane
       ? this.$splitter.getPaneSize(this) + 'px'
       : (this.$splitter.getPaneSize(this) - this.$splitter.handleSize) + 'px'
 
-    this.style[store.from] = this.$splitter.getPanePosition(this) + 'px'
+    this.style[posProp] = this.$splitter.getPanePosition(this) + 'px'
     if (!isFirstPane) {
-      this.style['padding-' + store.from] = this.$splitter.handleSize + 'px'
+      this.style['padding-' + posProp] = this.$splitter.handleSize + 'px'
     }
 
     // 设置 handle
     this.$handle.style.width = this.$handle.style.height = ''
-    this.$handle.style[store.prop] = isFirstPane ? '0' : this.$splitter.handleSize + 'px'
+    this.$handle.style[sizeProp] = isFirstPane ? '0' : this.$splitter.handleSize + 'px'
   }
 
   render() {}
@@ -673,7 +631,6 @@ export class BlocksSplitterPane extends HTMLElement {
     this.expandPane(this)
   }
 }
-
 
 if (!customElements.get('bl-splitter')) {
   customElements.define('bl-splitter', BlocksSplitter)
