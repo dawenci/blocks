bbdc23d521ea60902558f12ecdfb23a46ca4cddb
dawenci <dawenci@gmail.com>
2021-04-14 23:54:58 +0800

    完善 image-viewer 组件。

--- components/image-viewer/index.js
+++ components/image-viewer/index.js
@@ -6,6 +6,7 @@ import { boolGetter, boolSetter, enumGetter, enumSetter } from '../../common/pro
 import { __fg_placeholder, __height_base, __transition_duration } from '../../theme/var.js'
 import { dispatchEvent } from '../../common/event.js'
 import { makeMessages } from '../../i18n/makeMessages.js'
+import { disabledSetter } from '../../common/propertyAccessor.js'
 
 const getMessage = makeMessages('image-viewer', {
 })
@@ -21,13 +22,20 @@ cssTemplate.textContent = `
   bottom: 0;
   left: 0;
   background: rgba(0,0,0,.5);
+  z-index: 10;
 }
+::slotted(*) {
+  display: none;
+}
+
 #layout {
+  overflow: hidden;
   width: 100%;
   height: 100%;
   position: relative;
 }
 #content {
+  overflow: hidden;
   width: 100%;
   height: 100%;
   position: absolute;
@@ -35,7 +43,17 @@ cssTemplate.textContent = `
   right: 0;
   bottom: 0;
   left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
+#active {
+  margin: 0;
+  max-width: 100%;
+  max-height: 100%;
+  transition: transform var(--transition-duration, ${__transition_duration});
+}
+
 #toolbar {
   position: absolute;
   top: 0;
@@ -45,22 +63,110 @@ cssTemplate.textContent = `
   width: 100%;
   height: 44px;
   background: rgba(0,0,0,.1);
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: flex-end;
 }
+
+.toolbar-button {
+  overflow: hidden;
+  width: 18px;
+  height: 18px;
+  margin: 0 12px;
+  padding: 0;
+  border: none;
+  background: none;
+}
+.toolbar-button:focus {
+  outline: none;
+}
+.toolbar-button bl-icon {
+  fill: #fff;
+  width: 100%;
+  height: 100%;
+  opacity: .7;
+}
+.toolbar-button:hover bl-icon {
+  opacity: 1;
+}
+.toolbar-button[disabled] bl-icon {
+  opacity: .2;
+}
+
 #prev,
 #next {
+  overflow: hidden;
   position: absolute;
   top: 0;
   bottom: 0;
+  margin: auto;
+  width: 44px;
+  height: 44px;
+  border-radius: 50%;
+  background-color: rgba(0,0,0,.1);
+  cursor: pointer;
 }
+#prev::before,
+#next::before {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 10px;
+  height: 10px;
+  border-top: 1px solid #fff;
+  border-right: 1px solid #fff;
+  opacity: .3;
+}
+#prev {
+  left: 12px;
+}
+#next {
+  right: 12px;
+}
+#prev::before {
+  left: 18px;
+  transform: rotate(-135deg);
+}
+#next::before {
+  right: 18px;
+  transform: rotate(45deg);
+}
+#prev:hover::before,
+#next:hover::before {
+  opacity: .8;
+}
+
+#prev[disabled]::before,
+#next[disabled]::before,
+#prev[disabled]:hover::before,
+#next[disabled]:hover::before {
+  opacity: .3;
+  cursor: default;
+}
+
 `
 
 const template = document.createElement('template')
 template.innerHTML = `
 <div id="layout">
-  <div id="content"><div>
-  <div id="toolbar"></div>
+  <div id="content">
+    <img id="active" />
+  <div>
+  <div id="thumbnails"></div>
+  <div id="toolbar">
+    <button class="toolbar-button" id="rotate-left"><bl-icon value="rotate-left"></bl-icon></button>
+    <button class="toolbar-button" id="rotate-right"><bl-icon value="rotate-right"></bl-icon></button>
+    <button class="toolbar-button" id="zoom-out"><bl-icon value="zoom-out"></bl-icon></button>
+    <button class="toolbar-button" id="zoom-in"><bl-icon value="zoom-in"></bl-icon></button>
+    <button class="toolbar-button" id="close"><bl-icon value="cross"></bl-icon></button>
+  </div>
   <div id="prev"></div>
   <div id="next"></div>
+  <slot></slot>
 </div>
 `
 
@@ -70,162 +176,114 @@ class BlocksImageViewer extends HTMLElement {
     return []
   }
 
+  get imgs() {
+    return this._imgs ?? []
+  }
+
+  set imgs(value) {
+    this._imgs = value ?? []
+    this.render()
+  }
+
+  get activeImg() {
+    return this._activeImg ?? this.imgs?.[0]
+  }
+
+  set activeImg(value) {
+    this._activeImg = value
+    this._renderCurrent()
+  }
+
   constructor() {
     super()
     const shadowRoot = this.attachShadow({mode: 'open'})
     shadowRoot.appendChild(cssTemplate.cloneNode(true))
     shadowRoot.appendChild(template.content.cloneNode(true))
 
-    this.$layout = shadowRoot.getElementById('layout')
-    this.$img = shadowRoot.getElementById('img')
+    this.$slot = shadowRoot.querySelector('slot')
+    this.$content = shadowRoot.getElementById('content')
+    this.$thumbnails = shadowRoot.getElementById('thumbnails')
+    this.$active = shadowRoot.getElementById('active')
+    this.$prev = shadowRoot.getElementById('prev')
+    this.$next = shadowRoot.getElementById('next')
+    this.$closeButton = shadowRoot.getElementById('close')
+    this.$rotateLeftButton = shadowRoot.getElementById('rotate-left')
+    this.$rotateRightButton = shadowRoot.getElementById('rotate-right')
+    this.$zoomInButton = shadowRoot.getElementById('zoom-in')
+    this.$zoomOutButton = shadowRoot.getElementById('zoom-out')
 
-    definePrivate(this, '_status', 'init')
+    this.imgMap = new Map()
 
-    this.$img.onload = () => {
-      if (this._status === 'loading') {
-        this._status = 'loaded'
-        this._renderSuccess()
-        dispatchEvent(this, 'loaded')
+    this.$slot.addEventListener('slotchange', e => {
+      const imgs = this.$slot.assignedElements().filter(el => el.nodeName === 'IMG')
+      const newMap = new Map()
+      imgs.forEach($img => {
+        if (this.imgMap.has($img)) {
+          newMap.set($img, this.imgMap.get($img))
+        }
+        else {
+          newMap.set($img, {
+            scale: 1,
+            rotate: 0,
+          })
+        }
+      })
+      this.imgMap = newMap
+      this.imgs = imgs
+    })
+
+    this.$prev.onclick = e => {
+      if (this.imgs.length && this.activeImg !== this.imgs[0]) {
+        const index = this.imgs.indexOf(this.activeImg)
+        this.activeImg = this.imgs[index - 1]
+
+        disabledSetter(this.$next, false)
+        if (index === 1) {
+          disabledSetter(this.$prev, true)
+        }
+      }
+    }
+    this.$next.onclick = e => {
+      if (this.imgs.length && this.activeImg !== this.imgs[this.imgs.length - 1]) {
+        const index = this.imgs.indexOf(this.activeImg)
+        this.activeImg = this.imgs[index + 1]
+
+        disabledSetter(this.$prev, false)
+        if (index === this.imgs.length - 2) {
+          disabledSetter(this.$next, true)
+        }        
       }
     }
 
-    this.$img.onerror = () => {
-      if (this._status === 'loading') {
-        this._status = 'error'
-        this._renderFail()
-        dispatchEvent(this, 'error')
+    this.$rotateLeftButton.onclick = e => {
+      if (!this.activeImg) return
+      this.imgMap.get(this.activeImg).rotate -= 90
+      this._renderCurrent()
+    }
+
+    this.$rotateRightButton.onclick = e => {
+      if (!this.activeImg) return
+      this.imgMap.get(this.activeImg).rotate += 90
+      this._renderCurrent()
+    }
+
+    this.$zoomInButton.onclick = e => {
+      if (!this.activeImg) return
+      this.imgMap.get(this.activeImg).scale += 1
+      this._renderCurrent()
+    }
+
+    this.$zoomOutButton.onclick = e => {
+      if (!this.activeImg) return
+      if (this.imgMap.get(this.activeImg).scale > 1) {
+        this.imgMap.get(this.activeImg).scale -= 1
       }
-    }
-  }
-
-  get alt() {
-    return this.getAttribute('alt')
-  }
-
-  set alt(value) {
-    this.setAttribute('alt', value)
-  }
-
-  get fallback() {
-    return this.getAttribute('fallback')
-  }
-
-  set fallback(value) {
-    this.setAttribute('fallback', value)
-  }
-
-  get manual() {
-    return boolGetter('manual')(this)
-  }
-
-  set manual(value) {
-    boolSetter('manual')(this, value)
-  }
-
-  get placeholder() {
-    return this.getAttribute('placeholder')
-  }
-
-  get fit() {
-    return enumGetter('fit', [null, 'none', 'fill', 'contain', 'cover', 'scale-down'])(this)
-  }
-
-  set fit(value) {
-    enumSetter('fit', [null, 'none', 'fill', 'contain', 'cover', 'scale-down'])(this, value)
-  }
-
-  set placeholder(value) {
-    this.setAttribute('placeholder', value)
-  }
-
-  get src() {
-    return this.getAttribute('src')
-  }
-
-  set src(value) {
-    this.setAttribute('src', value)
-  }
-
-  _renderLoading() {
-    this.$img.style.opacity = '0'
-    this._removeFallback()
-
-    if (!this.$placeholder) {
-      this.$placeholder = this.$layout.appendChild(placeholderTemplate.content.cloneNode(true).querySelector('#placeholder'))
-      this.$placeholder.querySelector('.placeholderText').textContent = getMessage('placeholderText')
-      if (this.placeholder) {
-        this.$placeholder.querySelector('img').src = this.placeholder
-      }
-    }
-  }
-
-  _renderFail() {
-    this.$img.style.opacity = '0'
-    this._removePlaceholder()
-
-    if (!this.$fallback) {
-      this.$fallback = this.$layout.appendChild(fallbackTemplate.content.cloneNode(true).querySelector('#fallback'))
-      this.$fallback.querySelector('.fallbackText').textContent = getMessage('fallbackText')
-      if (this.fallback) {
-        this.$fallback.querySelector('img').src = this.fallback
-      }
-    }
-  }
-
-  _renderSuccess() {
-    this.$img.style.opacity = '1'
-    this._removePlaceholder()
-    this._removeFallback()
-
-    if (this.fit) {
-      this.$img.style.objectFit = this.fit
-    }
-  }
-
-  _removePlaceholder() {
-    if (this.$placeholder) {
-      this.$layout.removeChild(this.$placeholder)
-      this.$placeholder = null
-    }
-  }
-
-  _removeFallback() {
-    if (this.$fallback) {
-      this.$layout.removeChild(this.$fallback)
-      this.$fallback = null
-    }
-  }
-
-  _reset() {
-    this._status = 'init'
-    this.$img.style.display = 'none'
-    this.$img.style.opacity = '0'
-    this.$img.src = ''
-    this.$img.style.display = ''
-    this._removePlaceholder()
-    this._removeFallback()
-  }
-
-  render() {
-    if (this.$img.getAttribute('alt') !== this.alt) {
-      this.$img.setAttribute('alt', this.alt)
+      this._renderCurrent()
     }
 
-    switch (this._status) {
-      case 'loading': return this._renderLoading()
-      case 'error': return this._renderFail()
-      case 'loaded': return this._renderSuccess()
+    this.$closeButton.onclick = e => {
+      
     }
-  }
-
-  load() {
-    if (this._status === 'loading' || (this.$img.getAttribute('src') === this.src && this._status === 'loaded')) {
-      return
-    }
-    this._status = 'loading'
-    this.$img.src = this.src
-    this._renderLoading()
-    dispatchEvent(this, 'loading')
   }
 
   connectedCallback() {
@@ -233,9 +291,6 @@ class BlocksImageViewer extends HTMLElement {
       upgradeProperty(this, attr)
     })
     this.render()
-    if (!this.manual) {
-      this.load()
-    }
   }
 
   disconnectedCallback() {}
@@ -243,17 +298,28 @@ class BlocksImageViewer extends HTMLElement {
   adoptedCallback() {}
 
   attributeChangedCallback(attrName, oldVal, newVal) {
-    if (attrName === 'src') {
-      if (!this.manual) {
-        this.load()
-      }
-      else {
-        this._reset()
-      }
+    this.render()
+  }
+
+  render() {
+    this._renderNavButton()
+    this._renderCurrent()
+  }
+
+  _renderCurrent() {
+    if (this.activeImg) {
+      const { scale, rotate } = this.imgMap.get(this.activeImg)
+      this.$active.src = this.activeImg.src
+      this.$active.style.transform = `scale3d(${scale}, ${scale}, 1) rotate(${rotate}deg)`
     }
-    else {
-      this.render()
-    }
+  }
+
+  _renderNavButton() {
+    const display = this.imgs.length > 1 ? 'block' : 'none'
+    this.$prev.style.display = this.$next.style.display = display
+
+    disabledSetter(this.$prev, !!this.activeImg && this.activeImg === this.imgs[0])
+    disabledSetter(this.$next, !!this.activeImg && this.activeImg === this.imgs[this.imgs.length - 1])
   }
 }
 
---
+++ icon/svg/zoom-out.svg.js
@@ -0,0 +1,9 @@
+import { getRegisteredSvgIcon, registerSvgIcon } from '../store.js'
+
+const data =
+`<?xml version="1.0" standalone="no"?>
+<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path d="M636.8 443.2h-312c-4 0-8 3.2-8 8v60c0 4.8 4 8 8 8h312c4.8 0 8-3.2 8-8v-60c0-4.8-3.2-8-8-8z m284 424L775.2 720.8c122.4-148.8 113.6-369.6-25.6-508.8C600.8 64 360.8 64 212 212 64 360.8 64 600.8 212 748.8c139.2 139.2 360 148 508.8 25.6l145.6 145.6c3.2 3.2 8 3.2 11.2 0l43.2-43.2c3.2-1.6 3.2-6.4 0-9.6zM696 696c-119.2 118.4-311.2 118.4-430.4 0-118.4-119.2-118.4-311.2 0-430.4C384 147.2 576.8 147.2 696 265.6c118.4 119.2 118.4 311.2 0 430.4z" p-id="12122"></path></svg>`
+
+registerSvgIcon('zoom-out', data)
+
+export default getRegisteredSvgIcon('zoom-out')
