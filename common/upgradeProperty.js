d6209defb2cc244f8d20cdb1f4aeccf68a4e3520
Xiaowen Ruan <dawenci@gmail.com>
2021-07-23 15:57:08 +0800

    新增导出函数

--- common/upgradeProperty.js
+++ common/upgradeProperty.js
@@ -3,6 +3,10 @@
 // 在用户使用一些框架加载组件时，可能回出现这种情况（组件定义 js 晚于自定义元素加载，customElements.define），
 // 因此需要进行属性升级，确保 setter 逻辑能工作，
 
+/**
+ * @param {HTMLElement} element
+ * @param {string} prop
+ */
 export function upgradeProperty(element, prop) {
   if (element.hasOwnProperty(prop)) {
     const value = element[prop]
